7f2c818edbbcdc0656606273846d1ddebef00a65
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2022-11-26 10:58:35 +0530

    Changes according to new request

--- main.py
+++ main.py
@@ -68,16 +68,19 @@ def reply():
         print("Inside Request json loads data")
         print(json.loads(request.data))
     
-    temp = json.loads(request.data)
-    print(temp)
-    print(temp['sourceAddress'])
-    sendText(temp['sourceAddress'], "en", "What is up?")
-    print(temp.get('sourceAddress'))
-    sendText(temp.get('sourceAddress'), "en", "Nothing is up!")
+    request_data = json.loads(request.data)
+    # if request_data.get('sourceCountry') is not None:
+    #     return ''
+    
+    print(request_data)
+    # print(request_data['sourceAddress'])
+    # sendText(request_data['sourceAddress'], "en", "What is up?")
+    # print(request_data.get('sourceAddress'))
+    # sendText(request_data.get('sourceAddress'), "en", "Nothing is up!")
 
-    # if "order" in temp["messageParameters"]:
-    #     WaId = temp["sourceAddress"]
-    #     if len(temp["product_items"]) < 1:
+    # if "order" in request_data["messageParameters"]:
+    #     WaId = request_data["sourceAddress"]
+    #     if len(request_data["product_items"]) < 1:
     #         print('No Course Selected')
     #         sendText(WaId,'en',"No Course Selected")
     #         pass
@@ -92,7 +95,7 @@ def reply():
     #             pass
     #         else:
     #             # Get Requested Courses from cart
-    #             requestedCourses = temp["product_items"]
+    #             requestedCourses = request_data["product_items"]
     #             alreadyRegisteredCourses = [x["courseId"] for x in userInfo["courses"]]
     #             print("already registered", alreadyRegisteredCourses)
     #             alreadyRegisteredFlag = 0
@@ -148,10 +151,10 @@ def reply():
     #                 # send payment link
     #                 sendText(WaId,'en',"http://localhost:5000/register-for-course/"+WaId)
     
-    return ''
+        # return ''
     
     #_________________________________________
-    if request.form.get('MediaContentType0' ) is not None:
+    if request_data['message']['image'] is not None:
         
         print(request.form)
        
@@ -170,12 +173,13 @@ def reply():
         
         print(textFromImage)
         print(google_search(textFromImage))
-        sendText(request.form.get('WaId'),'en',google_search(textFromImage))
+        sendText(request_data['from'],'en',google_search(textFromImage))
         return ''
-    print(request)
+    # print(request)
     global quiz_time
-    message_ = request.form.get('Body')
-    print(request.form)
+    message_ = request_data['message']['text']['body']
+    # print(request.form)
+
     langId = 'en'
     # if langid.classify(message_) is None:
     #     langId = 'en'
@@ -203,17 +207,17 @@ def reply():
         mediaId,mediaType = uploadMedia('ytImage.jpg','ytImage.jpg','jpg')
         print(mediaId)
         url_link = '\n' + ytResult['url'] + '\n'
-        # sendTemplateForYoutube(request.form.get('WaId'),mediaId,mediaType,""" URL    sdfghbj""")
+        # sendTemplateForYoutube(request_data['from'],mediaId,mediaType,""" URL    sdfghbj""")
         print()
 
 #_______________________________   Video Search Send Template Ends  ________________________________________________________________
 
 
-    user = db['test'].find_one({'_id': request.form.get('WaId')})
+    user = db['test'].find_one({'_id': request_data['from']})
     print(response_df.query_result.intent.display_name)
     if user == None and response_df.query_result.intent.display_name != 'Register' and response_df.query_result.intent.display_name != 'Organisation':
         # send button to register
-        # sendTwoButton(request.form.get('WaId'), "Welcome to our world of education", "register", "I want to register right now!", "surf",  "I am just here to surf and explore!")
+        # sendTwoButton(request_data['from'], "Welcome to our world of education", "register", "I want to register right now!", "surf",  "I am just here to surf and explore!")
         welcome_text = ["Welcome to our world of education",
                         "It's a better place if you register today!",
                         "Trust me! Registering with us will brighten your future",
@@ -221,21 +225,20 @@ def reply():
         print(message)
         # print(response_df.query_result.language_code)
 
-        sendTwoButton(request.form.get('WaId'), langId, welcome_text[random.randint(0, 3)], ["register", "roam"], ["Register right now!", "Just exploring!"])
+        sendTwoButton(request_data['from'], langId, welcome_text[random.randint(0, 3)], ["register", "roam"], ["Register right now!", "Just exploring!"])
         return '' 
 
     if user == None and (response_df.query_result.intent.display_name == 'Register' or response_df.query_result.intent.display_name == 'Register-Follow'):
-        db["test"].insert_one({'_id': request.form.get(
-            'WaId'), 'name': '', 'email': '', 'langId': langId})
-        sendText(request.form.get('WaId'), langId, response_df.query_result.fulfillment_text)
+        db["test"].insert_one({'_id': request_data['from'], 'name': '', 'email': '', 'langId': langId})
+        sendText(request_data['from'], langId, response_df.query_result.fulfillment_text)
         return ''
 
     if user == None and response_df.query_result.intent.display_name == 'Organisation':
-        organisationIntroduction(request.form.get('WaId'), langId)
+        organisationIntroduction(request_data['from'], langId)
         return ''
 
     if user == None and response_df.query_result.intent.display_name == 'Organisation - history' or response_df.query_result.intent.display_name == 'Organisation - vision' or response_df.query_result.intent.display_name == 'Organisation - visit':
-        sendText(request.form.get('WaId'), langId, response_df.query_result.fulfillment_text)
+        sendText(request_data['from'], langId, response_df.query_result.fulfillment_text)
         return ''
 
     if user != None and (response_df.query_result.intent.display_name == 'Register' or response_df.query_result.intent.display_name == 'Register-Follow' or response_df.query_result.intent.display_name == 'Enroll-Courses' ):
@@ -243,18 +246,18 @@ def reply():
             name_ = str(response_df.query_result.output_contexts[0].parameters.fields.get(
                 'person.original'))
             name = name_.split("\"")[1]
-            db['test'].update_one({'_id': request.form.get('WaId')}, {"$set": {'name': name}})
-            sendText(request.form.get('WaId'), user['langId'], response_df.query_result.fulfillment_text)
+            db['test'].update_one({'_id': request_data['from']}, {"$set": {'name': name}})
+            sendText(request_data['from'], user['langId'], response_df.query_result.fulfillment_text)
             return ''
 
         elif user['email'] == '':
             email_ = str(response_df.query_result.output_contexts[0].parameters.fields.get(
                 'email.original'))
             email = email_.split("\"")[1]
-            db['test'].update_many({'_id': request.form.get('WaId')}, {"$set": {'email': email.lower(), 'scheduleDone': "false"}})
-            sendText(request.form.get('WaId'), user['langId'], response_df.query_result.fulfillment_text)
-            # sendText(request.form.get('WaId'), user['langId'],"Please find below the courses we offer")
-            # sendList(request.form.get('WaId'),user['langId'],"These are the courses We offer , Please select one","Courses",["Math","Science","History","Geography","English"],["Math-Course","Science-Course","History-Course","Geography-Course","English"],["NCERT + JEE + CET ","Arihant + JEE","Textbook questions + Revision","Textbook questions + Revision","English grammer + Writing skills and much more..."])
+            db['test'].update_many({'_id': request_data['from']}, {"$set": {'email': email.lower(), 'scheduleDone': "false"}})
+            sendText(request_data['from'], user['langId'], response_df.query_result.fulfillment_text)
+            # sendText(request_data['from'], user['langId'],"Please find below the courses we offer")
+            # sendList(request_data['from'],user['langId'],"These are the courses We offer , Please select one","Courses",["Math","Science","History","Geography","English"],["Math-Course","Science-Course","History-Course","Geography-Course","English"],["NCERT + JEE + CET ","Arihant + JEE","Textbook questions + Revision","Textbook questions + Revision","English grammer + Writing skills and much more..."])
             return ''
         
 
@@ -264,7 +267,7 @@ def reply():
     # quiz_count = user['quiz_count']
     quiz_count = 100
     print("HELLLLLOCOCOCOCOCO")
-    # message = request.form.get('Body').lower()
+    # message = request_data['message']['text']['body'].lower()
     if quiz_time and quiz_count == 0:
         quiz_initial(user, quiz_count)
         return ''
@@ -276,7 +279,7 @@ def reply():
 def quiz_initial(user, quiz_count):
     quiz_count = quiz_count + 1
     quiz_bot2(db, 'M1', quiz_count)
-    db['test'].update_one({'_id': request.form.get('WaId')}, {"$set": {'quiz_count': quiz_count}})
+    db['test'].update_one({'_id': request_data['from']}, {"$set": {'quiz_count': quiz_count}})
     print('COUNT ' + str(quiz_count))
     return ''
 
@@ -289,7 +292,7 @@ def quiz_bot2(db, quizID, questionNumber):
         # send_message(quiz[questionNumberString]['question'], '')
         # options = '\n' + quiz[questionNumberString]['A'] + '\n' + quiz[questionNumberString]['B'] + '\n' + quiz[questionNumberString]['C'] + '\n' + quiz[questionNumberString]['D'] + '\n'
         # send_message(options, '')
-        sendQuiz(request.form.get('WaId'), quiz, questionNumberString)
+        sendQuiz(request_data['from'], quiz, questionNumberString)
 
     if questionNumber > 1 and questionNumber < 7:
         questionNumberString = str(questionNumber - 1)
@@ -302,17 +305,17 @@ def quiz_chat(user, user_answer):
     global quiz_time
     quiz_count = user['quiz_count']
     quiz_count = quiz_count + 1
-    db['test'].update_one({'_id': request.form.get('WaId')}, {"$set": {'quiz_count': quiz_count}})
+    db['test'].update_one({'_id': request_data['from']}, {"$set": {'quiz_count': quiz_count}})
     print(quiz_count)
     previous_answer = quiz_bot2(db, 'M1', quiz_count)
     if user_answer == previous_answer:
         quiz_marks = user['quizzes']['M1'] + 2
         print(quiz_marks)
-        db['test'].update_one({'_id': request.form.get('WaId')}, {"$set": {'quizzes.M1': quiz_marks}})
+        db['test'].update_one({'_id': request_data['from']}, {"$set": {'quizzes.M1': quiz_marks}})
     if quiz_count == 6 or quiz_count > 6:
         quiz_time = False
-        db['test'].update_one({'_id': request.form.get('WaId')}, {"$set": {'quiz_count': 0}})
-        sendText(request.form.get('WaId'), 'Your quiz is over!')
+        db['test'].update_one({'_id': request_data['from']}, {"$set": {'quiz_count': 0}})
+        sendText(request_data['from'], 'Your quiz is over!')
         return ''
     else:
         return ''
@@ -321,56 +324,56 @@ def quiz_chat(user, user_answer):
 def workflow(user, request, response_df):
     global quiz_time
     if quiz_time:
-        # user = db['test'].find_one({'_id': request.form.get('WaId')})
+        # user = db['test'].find_one({'_id': request_data['from']})
         quiz_answer = db['course']
-        quiz_chat(user, request.form.get('Body'))
+        quiz_chat(user, request_data['message']['text']['body'])
         return ''
 
     if not quiz_time:
-        # message = request.form.get('Body').lower() # video on digimon
+        # message = request_data['message']['text']['body'].lower() # video on digimon
         # response_df = dialogflow_query(message)
         
         if response_df.query_result.intent.display_name == 'Organisation':
-            organisationIntroduction(request.form.get('WaId'), user['langId'])
+            organisationIntroduction(request_data['from'], user['langId'])
             return ''
         
         if response_df.query_result.intent.display_name == 'Organisation - history' or response_df.query_result.intent.display_name == 'Organisation - vision' or response_df.query_result.intent.display_name == 'Organisation - visit':
-            sendText(request.form.get('WaId'), user['langId'], response_df.query_result.fulfillment_text)
+            sendText(request_data['from'], user['langId'], response_df.query_result.fulfillment_text)
             return ''
         
         if response_df.query_result.intent.display_name == 'Schedule':
             timeSlots = getTimeSlot()
             print(timeSlots)
-            sendList(request.form.get('WaId'), user["langId"], "Please choose your preferred time for tomorrow!", "Free slots tomorrow!", timeSlots, timeSlots, None)
+            sendList(request_data['from'], user["langId"], "Please choose your preferred time for tomorrow!", "Free slots tomorrow!", timeSlots, timeSlots, None)
             return '' 
         
         if response_df.query_result.intent.display_name == 'Schedule - time':
-            bookTimeSlot(request.form.get('Body'), request.form.get('WaId'), user['langId'])
+            bookTimeSlot(request_data['message']['text']['body'], request_data['from'], user['langId'])
             return ''
         
         if response_df.query_result.intent.display_name == 'Schedule - time - yes' or response_df.query_result.intent.display_name == 'Schedule - time - no':
             desiredTime_ = str(response_df.query_result.output_contexts[0].parameters.fields.get('time.original'))
             desiredTime = desiredTime_.split("\"")[1]
-            rescheduleAppointment(response_df.query_result.intent.display_name, request.form.get('WaId'), user['langId'], desiredTime)
+            rescheduleAppointment(response_df.query_result.intent.display_name, request_data['from'], user['langId'], desiredTime)
             return ''
 
         if user['scheduleDone'] == 'false':
-            # sendTwoButton(request.form.get('WaId'), user["langId"], "Why not explore the courses we offer? \n You can also know more about us!", ["courses", "organisation"], ["Explore courses now!", "Know more about us!"])
-            # studentProgress(request.form.get('WaId'))
+            # sendTwoButton(request_data['from'], user["langId"], "Why not explore the courses we offer? \n You can also know more about us!", ["courses", "organisation"], ["Explore courses now!", "Know more about us!"])
+            # studentProgress(request_data['from'])
             print("Working !")
-            # sendText(request.form.get('WaId'), user['langId'], 'https://a837-115-96-217-68.ngrok.io/register-for-course/'+request.form.get('WaId'))
+            # sendText(request_data['from'], user['langId'], 'https://a837-115-96-217-68.ngrok.io/register-for-course/'+request_data['from'])
             return ''
 
         if response_df.query_result.intent.display_name == 'Videos':
             result_videos = youtube(response_df.query_result.query_text)
             print(result_videos)
             for video in result_videos:
-                sendText(request.form.get('WaId'),video['url'] + ' | ' + video['title'])
+                sendText(request_data['from'],video['url'] + ' | ' + video['title'])
             return ''
 
         if response_df.query_result.intent.display_name == 'WebSearch':  # Google JEE datde
             result_search = google_search(response_df.query_result.query_text)
-            sendText(request.form.get('WaId'), result_search)
+            sendText(request_data['from'], result_search)
 
         if response_df.query_result.intent.display_name == 'Parent':
             print(response_df.query_result.parameters)
@@ -384,12 +387,12 @@ def workflow(user, request, response_df):
             now = datetime.datetime.now()
             print(now.year, now.month, now.day,now.hour, now.minute, now.second)
             print(type(now.year), type(now.month), type(now.day),type(now.hour), type(now.minute), type(now.second))
-            print(request.form.get('From'))
+            # print(request.form.get('From'))
             # send_message(request.form.get('From'), response_df.query_result.fulfillment_text,'')
             print(response_df.query_result.fulfillment_text)
             print(response_df.query_result.intent.display_name)
-            print(request.form)
-            sendText(request.form.get('WaId'), user['langId'], response_df.query_result.fulfillment_text)
+            # print(request.form)
+            sendText(request_data['from'], user['langId'], response_df.query_result.fulfillment_text)
 
     return ''
 
